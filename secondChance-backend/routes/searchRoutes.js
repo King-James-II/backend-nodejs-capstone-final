8ae066d4e37f2db41a65846049ec86757fd42933
King-james-II <jocundjames@gmail.com>
2024-06-14 05:31:50 -0400

    Linted more files

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -4,38 +4,37 @@ const connectToDatabase = require('../models/db')
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
-
   try {
     // Connect to MongoDB using connectToDatabase database. Remember to use
     // the await keyword and store the connection in `db`
     const db = await connectToDatabase()
     const collection = db.collection(process.env.MONGO_COLLECTION)
     // Initialize the query object
-    let query = {};
+    const query = {}
     // Add the name filter to the query if the name parameter is not empty
     if (req.query.name && req.query.name.trim() !== '') {
       // Using regex for partial match, case-insensitive
-      query.name = { $regex: req.query.name, $options: "i" }; 
+      query.name = { $regex: req.query.name, $options: 'i' }
     }
 
     // Add other filters to the query
     if (req.query.category) {
-      query.category = req.query.category;
+      query.category = req.query.category
     }
     if (req.query.condition) {
-      query.condition = req.query.condition;
+      query.condition = req.query.condition
     }
     if (req.query.age_years) {
-      query.age_years = { $lte: parseInt(req.query.age_years) };
+      query.age_years = { $lte: parseInt(req.query.age_years) }
     }
 
-    // Fetch filtered gifts using the find(query) method. Make sure to use 
-    //await and store the result in the `gifts` constant
+    // Fetch filtered gifts using the find(query) method. Make sure to use
+    // await and store the result in the `gifts` constant
     const gifts = await collection.find(query).toArray()
-    res.json(gifts);
+    res.json(gifts)
   } catch (e) {
-    next(e);
+    next(e)
   }
-});
+})
 
 module.exports = router
